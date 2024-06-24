ceccfb3a17c47b3be885f9c24900522e15d96590
tochi-joy <nzrkwetochi@gmail.com>
2024-06-24 15:41:02 +0100

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-   <h2> portfoio project...</h2>
+   <Navbar/>
+
+   
   );
 }
 
